ea72b89dcd19d6a27fa3f7335312ae4f49b81af6
FranciscoThiesen <franciscogthiesen@gmail.com>
2019-04-09 12:20:42 -0300

    First attempt at printing transactions as they are processed

--- Mce/DataStore.py
+++ Mce/DataStore.py
@@ -1318,6 +1318,7 @@ store._ddl['txout_approx'],
                 VALUES (?, ?, ?)""",
                       (block_id, tx['tx_id'], tx_pos))
             store.log.info("Testando!! block_tx %d %d", block_id, tx['tx_id'])
+            store.log.info(tx)
 
         if b['height'] is not None:
             store._populate_block_txin(block_id)
