6a8cb291ff67e6ddbb2327d04419f67311547339
FranciscoThiesen <franciscogthiesen@gmail.com>
2019-04-09 12:42:06 -0300

    Minor changes

--- Mce/DataStore.py
+++ Mce/DataStore.py
@@ -1318,7 +1318,7 @@ store._ddl['txout_approx'],
                 VALUES (?, ?, ?)""",
                       (block_id, tx['tx_id'], tx_pos))
             store.log.info("Testando!! block_tx %d %d", block_id, tx['tx_id'])
-            print(tx)
+            store.log.info(type(tx) )
 
         if b['height'] is not None:
             store._populate_block_txin(block_id)
