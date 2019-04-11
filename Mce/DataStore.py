12a0c98dace60e5d1dff92550ec2bbf0350ae9cf
FranciscoThiesen <franciscogthiesen@gmail.com>
2019-04-11 20:42:36 -0300

    adapting to python version

--- Mce/DataStore.py
+++ Mce/DataStore.py
@@ -1318,9 +1318,9 @@ store._ddl['txout_approx'],
                 VALUES (?, ?, ?)""",
                       (block_id, tx['tx_id'], tx_pos))
             store.log.info("Testando!! block_tx %d %d", block_id, tx['tx_id'])
-            print("Imprimindo a transacao:")
-            # Special print
-            print(type(tx))
+            print("Imprimindo a transacao de forma diferente")
+            for k in tx.keys():
+                print(k, tx[k], type(k))
             
         if b['height'] is not None:
             store._populate_block_txin(block_id)
