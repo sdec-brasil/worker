e45f9dc0941facda07606578f50f5f68fb2cd299
Tiago Loriato <tloriatosimoes@gmail.com>
2019-07-25 02:11:24 -0300

    Trying to read new args 0.1

--- Mce/abe.py
+++ Mce/abe.py
@@ -224,6 +224,7 @@ class Abe:
     def __init__(abe, store, args):
         abe.store = store
         abe.args = args
+        print(abe.args)
         abe.htdocs = args.document_root or find_htdocs()
         abe.static_path = '' if args.static_path is None else args.static_path
         abe.template_vars = args.template_vars.copy()
